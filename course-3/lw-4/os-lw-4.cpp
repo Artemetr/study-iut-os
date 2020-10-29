06a3d15bd0c7f2d86c1fc85514b1bede0decded9
artemetr <artem@shirokikh.ru>
2020-10-29 10:41:07 +0500

    Template of lab

---
+++ course-3/lw-4/os-lw-4.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <thread>
+#include <pthread.h>
+
+using namespace std;
+
+void initUserMatrix(int**& userMatrix, int& size) {
+    int value;
+
+    cout << "Input matrix size: ";
+    cin >> size;
+    if (!size) {
+        throw "Invalid matrix size";
+    }
+
+    userMatrix = new int*[size];
+    for (int i = 0; i < size; i++) {
+        userMatrix[i] = new int[size];
+        for (int j = 0; j < size; j++) {
+            cin >> value;
+            if (!value) {
+                throw "Invalid matrix value";
+            }
+            userMatrix[i][j] = value;
+        }
+    }
+}
+
+void calculateBorderElementsSummOfMatrix(int** userMatrix, int size, int& result) {
+    result = 0;
+    for (int i = 0; i < size; i++) {
+        if (i == 0 || i == size - 1) {
+            for (int j = 0; j < size; j++) {
+                result += userMatrix[i][j];
+            }
+        } else {
+            result += userMatrix[i][0] + userMatrix[i][size - 1];
+        }
+    }
+}
+
+int main() {
+    int** userMatrix;
+    int result;
+    int size;
+
+    try {
+        initUserMatrix(userMatrix, size);
+    } catch (const char* e) {
+        cout << "Error: " << e << endl;
+        return EXIT_FAILURE;
+    }
+
+    thread th(calculateBorderElementsSummOfMatrix, userMatrix, size, ref(result));
+    sched_param sch;
+    int policy; 
+    pthread_getschedparam(th.native_handle(), &policy, &sch);
+    if (!th.joinable()) {
+        cout << "Error with thread starting." << endl;
+        return EXIT_FAILURE;
+    }
+    th.join();
+
+    if (!result) {
+        cout << "Error with invalide thread response." << endl;
+        return EXIT_FAILURE;
+    }
+
+    cout << "Summ of matrix border elements: " << result << endl;
+
+    return EXIT_SUCCESS;
+}
